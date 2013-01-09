d27a536771d0bece50624084be51b81f27d6aeb3
RAMON LENCE <rlence86@gmail.com>
2013-01-09 10:15:11 +0100

    Limpieza de código

--- src/java/com/master/agenda/utils/ConstantesAgenda.java
+++ src/java/com/master/agenda/utils/ConstantesAgenda.java
@@ -5,6 +5,7 @@ package com.master.agenda.utils;
  * @author Ramón Lence <rlence86@gmail.com>
  */
 public class ConstantesAgenda {
+    public final static String OPERACION_REGISTRAR_USUARIO = "0";
     public final static String OPERACION_VER_TODOS_CONTACTOS = "1";
     public final static String OPERACION_BUSCAR_UN_CONTACTO = "2";
     public final static String OPERACION_ANADIR_CONTACTO_NUEVO = "3";
--- src/java/com/master/agenda/web/ServletAgenda.java
+++ src/java/com/master/agenda/web/ServletAgenda.java
@@ -39,30 +39,14 @@ public class ServletAgenda extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
         try {
-            //Comprobar si tiene la cookie de usuario almacenada
-            Cookie[] cookies = request.getCookies();
-            boolean cookieActiva = false;
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("nombreUsuario")) {
-                    cookieActiva = true;
-                }
-            }
+            boolean cookieActiva = chequearRegistrado(request);
             //Si la cookie nombreUsuario está activa, se redirige a la página que se busca
             String operacionSeleccionada = request.getParameter("op");
             if (cookieActiva) {
                 if (operacionSeleccionada.equals(ConstantesAgenda.OPERACION_VER_TODOS_CONTACTOS)) {
-                    //Ver todos los contactos
-                    //Buscar todos los contactos
-                    List<Contacto> contactos = ContactoDAO.getInstance().consultarTodos();
-                    //Añadir todos los contactos al request
-                    request.setAttribute(ConstantesAgenda.ATRIBUTO_CONTACTOS, contactos);
-                    //Redirigir petición para mostrar los contactos
-                    RequestDispatcher rd = request.getRequestDispatcher("/verContactos.jsp");
-                    rd.forward(request, response);
+                    verTodosContactos(request, response);
                 } else if (operacionSeleccionada.equals(ConstantesAgenda.OPERACION_BUSCAR_UN_CONTACTO)) {
-                    //Mostrar formulario de búsqueda de usuario
-                    RequestDispatcher rd = request.getRequestDispatcher("/formularioBusqueda.jsp");
-                    rd.forward(request, response);
+                    mostrarFormularioBusqueda(request, response);
                 } else if (operacionSeleccionada.equals(ConstantesAgenda.OPERACION_ANADIR_CONTACTO_NUEVO)) {
                     //Añadir contacto nuevo
                 } else if (operacionSeleccionada.equals(ConstantesAgenda.OPERACION_ELIMINAR_CONTACTO)) {
@@ -70,42 +54,17 @@ public class ServletAgenda extends HttpServlet {
                 } else if (operacionSeleccionada.equals(ConstantesAgenda.OPERACION_MODIFICAR_CONTACTO)) {
                     //Modificar contacto
                 } else if (operacionSeleccionada.equals(ConstantesAgenda.OPERACION_BUSCAR_CON_NOMBRE)) {
-                    //Buscar con el nombre de usuario
-                    //Obtener el nombre de contacto a mostrar
-                    String nombreContacto = request.getParameter("nombreContacto");
-                    //Buscar contacto por nombre
-                    List<Contacto> contactos = new ArrayList<Contacto>();
-                    if(!nombreContacto.equals("")){
-                        contactos.add(ContactoDAO.getInstance().buscarContacto(nombreContacto));
-                    } else {
-                        //Si está vacío, se devuelve al formulario
-                        RequestDispatcher rd = request.getRequestDispatcher("/formularioBusqueda.jsp");
-                        rd.forward(request, response);
-                    }                    
-                    //Añadir resultado a request
-                    request.setAttribute(ConstantesAgenda.ATRIBUTO_CONTACTOS, contactos);
-                    //Redirigir petición para mostrar los contactos
-                    RequestDispatcher rd = request.getRequestDispatcher("/verContactos.jsp");
-                    rd.forward(request, response);
+                    buscarUnContacto(request, response);
                 } else {
                     //Otra operación (error)
                 }
                 //Si la cookie nombreUsuario no está activa, se redirige al formulario de login
             } else {
                 //Comprobamos si se está recibiendo el nombre de usuario desde login
-                if (operacionSeleccionada.equals("0")) {
-                    //Si la operación es 0, se reciben los datos desde login
-                    String nombreUsuario = request.getParameter("nombreUsuario");
-                    //Se crea la cookie con el valor recogido del formulario
-                    Cookie cookie = new Cookie("nombreUsuario", nombreUsuario);
-                    //Se le indica un máximo de duración y se añade a response
-                    int unaSemana = 7*24*60*60;
-                    cookie.setMaxAge(unaSemana);
-                    response.addCookie(cookie);
-                    //Se vuelve al índice
-                    RequestDispatcher rd = request.getRequestDispatcher("/index.jsp");
-                    rd.forward(request, response);
+                if (operacionSeleccionada.equals(ConstantesAgenda.OPERACION_REGISTRAR_USUARIO)) {
+                    registrarUsuario(request, response);
                 } else {
+                    //Se redirige a login
                     RequestDispatcher rd = request.getRequestDispatcher("/login.jsp");
                     rd.forward(request, response);
                 }
@@ -155,4 +114,65 @@ public class ServletAgenda extends HttpServlet {
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
+
+    private void verTodosContactos(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        //Ver todos los contactos
+        //Buscar todos los contactos
+        List<Contacto> contactos = ContactoDAO.getInstance().consultarTodos();
+        //Añadir todos los contactos al request
+        request.setAttribute(ConstantesAgenda.ATRIBUTO_CONTACTOS, contactos);
+        //Redirigir petición para mostrar los contactos
+        RequestDispatcher rd = request.getRequestDispatcher("/verContactos.jsp");
+        rd.forward(request, response);
+    }
+
+    private void mostrarFormularioBusqueda(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        //Mostrar formulario de búsqueda de usuario
+        RequestDispatcher rd = request.getRequestDispatcher("/formularioBusqueda.jsp");
+        rd.forward(request, response);
+    }
+
+    private void buscarUnContacto(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //Buscar con el nombre de usuario
+        //Obtener el nombre de contacto a mostrar
+        String nombreContacto = request.getParameter("nombreContacto");
+        //Buscar contacto por nombre
+        List<Contacto> contactos = new ArrayList<Contacto>();
+        if(!nombreContacto.equals("")){
+            contactos.add(ContactoDAO.getInstance().buscarContacto(nombreContacto));
+        } else {
+            mostrarFormularioBusqueda(request, response);
+        }                    
+        //Añadir resultado a request
+        request.setAttribute(ConstantesAgenda.ATRIBUTO_CONTACTOS, contactos);
+        //Redirigir petición para mostrar los contactos
+        RequestDispatcher rd = request.getRequestDispatcher("/verContactos.jsp");
+        rd.forward(request, response);
+    }
+
+    private boolean chequearRegistrado(HttpServletRequest request) {
+        //Comprobar si tiene la cookie de usuario almacenada
+        Cookie[] cookies = request.getCookies();
+        boolean cookieActiva = false;
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("nombreUsuario")) {
+                cookieActiva = true;
+            }
+        }
+        return cookieActiva;
+    }
+
+    private void registrarUsuario(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        //Si la operación es 0, se reciben los datos desde login
+        String nombreUsuario = request.getParameter("nombreUsuario");
+        //Se crea la cookie con el valor recogido del formulario
+        Cookie cookie = new Cookie("nombreUsuario", nombreUsuario);
+        //Se le indica un máximo de duración y se añade a response
+        int unaSemana = 7*24*60*60;
+        cookie.setMaxAge(unaSemana);
+        response.addCookie(cookie);
+        //Se vuelve al índice
+        RequestDispatcher rd = request.getRequestDispatcher("/index.jsp");
+        rd.forward(request, response);
+    }
 }
